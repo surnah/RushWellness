35f548335a1ccbe313f49f725411948b7e366329
surnah <64102688+surnah@users.noreply.github.com>
2020-04-21 14:54:23 -0500

    Rush Wellness Assistance Program
    RWAP Portal Admin TEST

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('.collapse.in').prev('.panel-heading').addClass('active');
+  $('#accordion, #accordion2, #accordion3')
+    .on('show.bs.collapse', function(a) {
+      $(a.target).prev('.panel-heading').addClass('active');
+    })
+    .on('hide.bs.collapse', function(a) {
+      $(a.target).prev('.panel-heading').removeClass('active');
+    });
+});
